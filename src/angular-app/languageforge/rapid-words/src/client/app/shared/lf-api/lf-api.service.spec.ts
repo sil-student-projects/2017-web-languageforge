395e4a3afba95b239caf88ea88237fb934ee099d
Nathaniel Paulus <nateowami@gmail.com>
2017-01-09 15:50:54 +0700

    Add basic Angular 2 LF API service

---
+++ src/angular-app/languageforge/rapid-words/src/client/app/shared/lf-api/lf-api.service.spec.ts
@@ -0,0 +1,10 @@
+import { MockBackend } from '@angular/http/testing'
+
+import { LFApiService } from './lf-api.service';
+
+export function main(){
+    describe('LFApi Service', () => {
+        let lfApiService: LFApiService;
+        let mockBackend: MockBackend;
+    });
+}
--- src/angular-app/languageforge/rapid-words/src/client/app/shared/shared.module.ts
+++ src/angular-app/languageforge/rapid-words/src/client/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NameListService } from './name-list/name-list.service';
+import { UserComponent } from './user/user.component'
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
@@ -13,8 +14,8 @@ import { NameListService } from './name-list/name-list.service';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [ToolbarComponent, NavbarComponent],
-  exports: [ToolbarComponent, NavbarComponent,
+  declarations: [ToolbarComponent, NavbarComponent, UserComponent],
+  exports: [ToolbarComponent, NavbarComponent, UserComponent,
     CommonModule, FormsModule, RouterModule]
 })
 export class SharedModule {
