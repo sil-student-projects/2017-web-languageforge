aab45fa85d5e1d36ff11d3384124f16d7ee166ad
Krista <krista.hapner@gmail.com>
2017-01-11 00:37:51 -0500

    updated the app module to reflect the new create-account, login,  project-select, and review modules

--- src/angular-app/languageforge/review-suggest/src/client/app/app.module.ts
+++ src/angular-app/languageforge/review-suggest/src/client/app/app.module.ts
@@ -8,9 +8,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AboutModule } from './about/about.module';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
+import { CreateAccountModule } from './create-account/create-account.module';
+import { LoginModule } from './login/login.module';
+import { ProjectSelectModule } from './project-select/project-select.module';
+import { ReviewModule } from './review/review.module';
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot()],
+  imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, CreateAccountModule, HomeModule, LoginModule, ProjectSelectModule, ReviewModule, SharedModule.forRoot()],
   declarations: [AppComponent],
   providers: [{
     provide: APP_BASE_HREF,
