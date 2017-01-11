ba9b36a1bed80de917575871bd58e88ea2c275f0
Taylor Dunlop <Taylor Dunlop>
2017-01-11 03:29:27 -0500

    Redone to get the Review Component working again. Followed the About Component almost systematically. Still need to do the same for the other components. Bootstrap styling falls apart and pages will likely need to be re-templated.

---
+++ src/angular-app/languageforge/review-suggest/src/client/app/review/review.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+/**
+ * This class represents the lazy loaded AboutComponent.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'sd-review',
+  templateUrl: 'review.component.html',
+  styleUrls: ['review.component.css']
+})
+export class ReviewComponent { }
