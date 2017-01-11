f19c69cb449a6c24acd1b07b945f447f7e0f3c31
Krista <krista.hapner@gmail.com>
2017-01-10 23:16:04 -0500

    fixed a typo in login routing

--- src/angular-app/languageforge/review-suggest/src/client/app/login/login-routing.module.ts
+++ src/angular-app/languageforge/review-suggest/src/client/app/login/login-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { LoginComponent } from './login-routing.component';
+import { LoginComponent } from './login.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: 'login-routing', component: LoginComponent }
+      { path: 'login', component: LoginComponent }
     ])
   ],
   exports: [RouterModule]
