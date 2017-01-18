8e23d6a666461893fa0ee473e55a2669972c3bd7
Joel Vastbinder <Joel Vastbinder>
2017-01-18 10:13:22 +0700

    Created a project service to grab list of user projects

---
+++ src/angular-app/languageforge/review-suggest/src/client/app/shared/services/project.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { LfApiService } from './lf-api.service';
+
+@Injectable()
+export class ProjectService {
+
+  constructor(private lfApiService: LfApiService) {
+    
+  }
+
+  getProjectList() {
+    return this.lfApiService.project_list().map(response => {
+      if (response.success) {
+        localStorage.setItem('current_user_projects', response.data.entries);
+      }
+      return response.data;
+    });
+  }
+  
+}
