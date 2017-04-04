bda957f4144cce281d162c55aa6d4171796c50ac
Stephen Enzor <stephenenzor94@gmail.com>
2017-04-04 14:35:17 -0400

    filtered list code; working on pipe

--- src/angular-app/languageforge/review-suggest/src/client/app/registerLangSelect/registerLangSelect-routing.module.ts
+++ src/angular-app/languageforge/review-suggest/src/client/app/registerLangSelect/registerLangSelect-routing.module.ts
@@ -2,6 +2,33 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RegisterLangSelectComponent } from './registerLangSelect.component';
 
+//TEST
+
+import { BrowserModule } from '@angular/platform-browser';
+//import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import {FilterPipe} from './pipes'
+
+//import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    RegisterLangSelectComponent,FilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [RegisterLangSelectComponent]
+})
+export class AppModule { }
+
+//END
+
 @NgModule({
   imports: [
     RouterModule.forChild([
--- src/angular-app/languageforge/review-suggest/src/client/app/registerLangSelect/registerLangSelect.component.ts
+++ src/angular-app/languageforge/review-suggest/src/client/app/registerLangSelect/registerLangSelect.component.ts
@@ -9,6 +9,23 @@ import { AuthService } from '../shared/services/auth.service';
 
 //TEST
 
+//import { Component } from '@angular/core';
+import {FilterPipe} from './pipes'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:String;
+  names:any;
+  constructor(){
+    this.title = "Angular 2 simple search";
+        this.names = ['Prashobh','Abraham','Anil','Sam','Natasha','Marry','Zian','karan']
+  }
+}
+
 //END
 
 @Component({
@@ -40,7 +57,6 @@ export class RegisterLangSelectComponent implements OnInit {
 
   onSubmit() {
 
-    //MY TEST CODE
     if(this.registerUser.password == this.registerUser.confPass) {
 
         var toastContent = '<b>Success! Passwords match</b>';
